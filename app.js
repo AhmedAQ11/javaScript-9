1d1a4e2ab0bc7e850c0def6a10ab3e7a3e6c29bc
AhmedAQ11 <m.ahmedAQ.786@gmail.com>
2021-11-23 02:45:50 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,88 @@
+
+//////// CHAPTER 43-48 ///////////
+
+/////// Q2 ///////////
+
+
+
+// function mobImg(){
+//     var mobPhone = document.getElementById("mobPhone")
+    
+//     mobPhone.innerHTML = "Thank for purchasing phone"
+
+//     alert(mobPhone.innerHTML)
+     
+// }
+
+
+
+////////////    Q3 /////////////
+
+
+// function tab(){
+//     var table = document.getElementById("table").deleteRow(-1);
+// }
+
+
+
+//////  Q4 ///////////////
+
+
+// function bulbOn(a){
+//         console.log(a)
+//          a.src = "./image/on.gif"
+//      }
+    
+//      function bulbOff(a){
+//         console.log("mouse")
+//        a.src = "./image/off.gif"
+//      }
+
+
+
+//////  Q5 /////////
+
+  // var click1 = 0;
+
+  // function clicks(){
+  //   var click = document.getElementById("click1")
+  //   click1 += 1;
+  //   click.innerHTML = click1;
+  // }
+
+
+
+
+
+  ///////////// CHAPTER 49-52 ////////////
+
+  ///////// Q1 ////////////
+
+
+// function signUp(){
+//   var user = document.getElementById("userText")
+//   var userPass = document.getElementById("userPass")
+  
+//   console.log( userText.value)
+//   console.log(userPass.value)
+  
+// }
+
+
+
+
+
+  ///////// Q2 ///////////////////
+
+
+//  function seeMore(){
+//   var newPara = document.getElementById("newPara");
+//   console.log(newPara)
+//   var hideText = document.getElementById("newText")
+//   newPara.innerHTML = newText.innerHTML
+  
+// }
+
+
+
+
